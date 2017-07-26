7f47965867ac96e159ee6369f997cda4fb0b57c0
m1g <miguelmalcolm@outlook.com>
2017-07-26 12:13:54 -0400

    added cart functionality

--- public/checkout.php
+++ public/checkout.php
@@ -1,14 +1,7 @@
 <?php require_once('../resources/config.php') ?>
+<?php require_once('cart.php') ?>
 
 <?php include(TEMPLATE_FRONT . DS . 'header.php') ?>
-
-<?php
-
-$_SESSION['product_1'] = 1
-
-
-
-?>
 
     <!-- Page Content -->
     <div class="container">
@@ -17,7 +10,7 @@ $_SESSION['product_1'] = 1
 <!-- /.row -->
 
 <div class="row">
-
+      <h4 class="text-center bg-danger"><?php display_message(); ?></h4>
       <h1>Checkout</h1>
 
 <form action="">
@@ -32,13 +25,7 @@ $_SESSION['product_1'] = 1
           </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>apple</td>
-                <td>$23</td>
-                <td>3</td>
-                <td>2</td>
-
-            </tr>
+            <?php cart(); ?>
         </tbody>
     </table>
 </form>
@@ -54,7 +41,9 @@ $_SESSION['product_1'] = 1
 
 <tr class="cart-subtotal">
 <th>Items:</th>
-<td><span class="amount">4</span></td>
+<td><span class="amount">
+  <?php echo isset($_SESSION['item_quantity']) ? $_SESSION['item_quantity'] : $_SESSION['item_quantity'] = ""; ?>
+</span></td>
 </tr>
 <tr class="shipping">
 <th>Shipping and Handling</th>
@@ -63,7 +52,11 @@ $_SESSION['product_1'] = 1
 
 <tr class="order-total">
 <th>Order Total</th>
-<td><strong><span class="amount">$3444</span></strong> </td>
+<td><strong><span class="amount">
+
+&#36;<?php echo isset($_SESSION['item_total']) ? $_SESSION['item_total'] : $_SESSION['item_total'] = "0"; ?>
+
+</span></strong> </td>
 </tr>
 
 
